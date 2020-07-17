afe118e701dbcd35324c4cc9530e7d8449bdc3f1
DaddyMaddy <madhurgupta744@gmail.com>
2020-07-17 19:15:01 +0530

    .

---
+++ Python/P1- Syntax.py
@@ -0,0 +1,8 @@
+print("Hello, World!")
+print("Hi Jatin")
+
+if 5 > 2:
+ print("Five is greater than two!") 
+if 5 > 2:
+                        print("Five is greater than two!")
+                                        print("Five is greater than two!")
---
+++ Python/P2- Comments.py
@@ -0,0 +1,8 @@
+#this symbol hashtag is used for commenting
+#for multiple lines we have to use hashtag for each line
+
+"""Also we can Use Triple Quotes for Commenting"""
+
+'''Can we use Triple Single quotes for commenting and the answer is yes'''
+
+"Yep"
